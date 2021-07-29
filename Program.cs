bfd3b67e3bae2f0f5920f83dc77ba16514571d7a
Austin Pierce <Austinentpierce@gmail.com>
2021-07-28 23:17:20 -0400

    Done with explorer mode.

--- Program.cs
+++ Program.cs
@@ -2,11 +2,54 @@
 
 namespace NumberGuesser
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+
+            {
+                Console.WriteLine("Hello I am the Number Guesser and I bet I can guess your number");
+                int lowestNumber = 1;
+                int highestNumber = 1024;
+                int DifferenceOfRange = highestNumber - lowestNumber;
+                int Midpoint = DifferenceOfRange / 2;
+                int middleNumberValue = lowestNumber + Midpoint;
+                //int Count = 1;
+                bool Number = true;
+                while (lowestNumber <= highestNumber && Number)
+                {
+                    DifferenceOfRange = highestNumber - lowestNumber;
+                    Midpoint = DifferenceOfRange / 2;
+                    middleNumberValue = lowestNumber + Midpoint;
+                    Console.WriteLine("Is your number: {0}?", middleNumberValue);
+                    Console.WriteLine(" 1: That is correct it is my number \n 2: Nope my number is actually greater than that \n 3: Nope my number is actually lower than that ");
+                    int numberGuessed = int.Parse(Console.ReadLine());
+                    if (numberGuessed == 1)
+                    {
+                        Console.WriteLine("Looks like I won! Better luck next time!");
+                        Number = false;
+                    }
+                    if (lowestNumber < highestNumber && numberGuessed == 2)
+                    {
+                        lowestNumber = middleNumberValue + 1;
+                        //Console.WriteLine("Your number is {0}", MidValue);
+                    }
+                    else if (lowestNumber < highestNumber && numberGuessed == 3)
+                    {
+                        highestNumber = middleNumberValue - 1;
+                        //Console.WriteLine("Your number is {0}", MidValue);
+                    }
+                }
+                {
+                }
+            }
+        }
     }
-  }
+
+
+
 }
+
+
+
+
